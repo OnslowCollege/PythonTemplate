9b83f83245d7bc5577294cbbadfbd9493f7d77d0
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-02-13 23:07:16 +0000

    Feb 13, 2024, 11:07 PM

---
+++ library_catalogue.py
@@ -0,0 +1,16 @@
+"""
+library_catalogue.
+
+Created by: Arkin
+Date: 14/02/2024
+"""
+
+class Book:
+    """Create books."""
+
+    details: str
+    title: str
+    auther: str
+    isbn: int
+
+    def
