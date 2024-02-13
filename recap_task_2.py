0f8eaf544b37909e4fe55b960182bd8882f6413d
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-02-13 00:07:01 +0000

    Feb 13, 2024, 12:07 AM

--- recap_task_2.py
+++ recap_task_2.py
@@ -18,32 +18,45 @@ name_counter: int = 1
 # Main loop
 while main_loop:
 
+    # Loop until valid reserve price is added
     while reserve_price < 0:
+
+        # Get the users input for reserve price
         try:
             reserve_price = float(input("What is the reserve price? ($): "))
 
+            # Check if the input was valid
             if reserve_price < 0:
                 print("Please enter a number 0 or above.")
             
+            # If it was valid
             else:
                 print("Action has started")
 
+        # If value was not a float/int
         except ValueError:
             print("Please enter a number 0 or above.")
 
+    # Start auction
     print(f"\nCurrent highest bid is ${highest_bid}")
 
+    # Get bidders name
     bidders_name = input("\nWhat is your name? ('F' to finish) ")
 
+    # If bidder already bid add counter
     if bidders_name in bidding_history:
         name_counter += 1
         bidders_name = bidders_name + str(name_counter)
 
+    # If you want auction to stop
     if bidders_name.lower() == "f":
         main_loop = False
 
+        # Print auction history from dictionary
         for bidder in bidding_history:
             print(f"{bidder}, bid {bidding_history[bidder]}$")
+
+    # If auction continues
     else:
         get_bid_amount = True
         while get_bid_amount:
