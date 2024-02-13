58d95c567fab90b4407350b9d6e6be9b7d21b240
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-02-13 23:09:12 +0000

    Feb 13, 2024, 11:09 PM

--- library_catalogue.py
+++ library_catalogue.py
@@ -7,10 +7,18 @@ Date: 14/02/2024
 
 class Book:
     """Create books."""
-
-    details: str
     title: str
-    auther: str
+    author: str
     isbn: int
+    availability: bool
 
-    def
+
+
+"Huwi's First Egg", by Kat Mereweather, ISBN: 123 (1 copy available)
+"The Kuia and the Spider", by Patricia Grace, ISBN: 124 (1 copy available)
+"Hairy Maclary from Donaldson's Dairy", by Lynley Dodd, ISBN: 125 (1 copy loaned)
+"Where the Wild Things Are", by Maurice Sendak, ISBN: 345 (2 copies available)
+"Under the Mountain", by Maurice Gee, ISBN: 346 (1 copy available)
+"Horrakapotchkin", by Margaret Mahy, ISBN: 347 (1 copy available)
+"Salmagundi", by Joy Cowley, ISBN: 409 (1 copy available, 1 copy loaned)
+"How Māui Found His Father and the Magic Jawbone", by Peter Gossage, ISBN: 410 (2 copies available)
