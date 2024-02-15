5f824e788db09ef50f778a03c1d287bf3d5266d9
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-02-15 21:02:11 +0000

    Feb 15, 2024, 9:02 PM

--- library.py
+++ library.py
@@ -13,11 +13,13 @@ from dataclasses import dataclass
 class Book:
     """Stores book values."""
 
+    # Values for book
     title: str
     author: str
     isbn: int
     availability: bool = True  # Default to available
 
+# Add some books thru book class
 books = [
     Book("Huwi's First Egg", "Kat Mereweather", 123, True),
     Book("The Kuia and the Spider", "Patricia Grace", 124, True),
@@ -33,48 +35,60 @@ class LibraryCatalogue:
     """Stores books in cataloge."""
 
     def __init__(self):
-        """Add the details to the book."""
+        """Add the details from the book."""
         self.books = books
 
+    # Function to add a book
     def add_book(self, book: Book):
         """Add a book function."""
         self.books.append(book)
 
+    # Function to remove a book
     def remove_book(self, isbn: int):
         """Remove a book from saved list."""
 
+        # Check book by isbn
         for index, book in enumerate(self.books):
             if book.isbn == isbn:
 
+                # Remove book
                 self.books.pop(index)
                 print(f"Book with ISBN {isbn} removed successfully.")
                 return
 
+        # If isbn was not found
         print(f"Book with ISBN {isbn} not found in library.")
 
+    # Search for book by title
     def search_by_title(self, title: str):
         """Search for books by title."""
 
+        # Check if book was found
         matches = [book for book in self.books if title.lower() \
                    in book.title.lower()]
 
+        # If book was found print book and details
         if matches:
             print(f"Found {len(matches)} books matching '{title}':")
             for book in matches:
 
                 print(f"  - {book.title} by {book.author} ({book.isbn})")
 
+                # If book is available or not print accordingly
                 if book.availability:
                     print("      - Available for issue")
                 else:
                     print("      - Currently unavailable")
 
+        # If no books were found that matches the title
         else:
             print(f"No books found matching '{title}'.")
 
+    # Search book by isbn and print details if can
     def search_by_isbn(self, isbn: int):
         """Look up book by entered isbn."""
 
+        # Get books details and check if one matches the isbn given
         for book in self.books:
             if book.isbn == isbn:
 
