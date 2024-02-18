c7d7f2d2cd59be76a758caf83f605023e6cebb8c
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-02-18 20:22:26 +0000

    Feb 18, 2024, 8:22 PM

--- instances_primm.py
+++ instances_primm.py
@@ -6,13 +6,26 @@ Date: 19/02/2024
 """
 
 class Film:
-    def __init__(self, title: str, director: str, year: int):
-       self.title = title
-       self.director = director
-       self.year_published = year_published
+    """Class that stores films and information regarding."""
 
-classic_film = Film("Gone with the Wind", "Victor Fleming", 1939)
-modern_film = Film("Barbie", "Greta Gerwig", 2023)
+    def __init__(self, title: str, director: str, year_published: int,
+                 genre: str, imbd_rating: float):
+        """Instance of film."""
+        self.title = title
+        self.director = director
+        self.year_published = year_published
+        self.genre = genre
+        self.imbd_rating = imbd_rating
+
+    def description(self):
+        """Print out film and description."""
+        print(f"{self.title} ({self.year_published}):"
+                f"\nDirector:{self.director}")
+
+classic_film = Film("Gone with the Wind", "Victor Fleming",
+                    1939, "Drama", 8.2)
+modern_film = Film("Barbie", "Greta Gerwig", 2023, "Comedy", 6.9)
 
 print(classic_film.title + " " + modern_film.director)
 
+classic_film.description()
