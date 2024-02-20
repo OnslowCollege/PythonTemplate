717638c245862d0df1ead70024b9b372cc95541c
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-02-20 22:58:43 +0000

    Feb 20, 2024, 10:58 PM

--- Creating instances of a class 2/Task_1.py
+++ Creating instances of a class 2/Task_1.py
@@ -4,3 +4,19 @@ Task_1.
 Created by: Arkin E
 Date: 20.02.2024
 """
+
+class BasketballPlayer:
+    def __init__(self, name: str, num_games_played: int, total_goals) -> None:
+        self.name = name
+        self.num_games_played = num_games_played
+        self.total_goals = total_goals
+
+    # agg is average goals per game
+    def calulate_agg(self):
+        return self.total_goals / self.num_games_played
+
+    def display_agerage(self):
+        print(f"Average goals per game: {self.calculate_agg}")
+
+player_1 = BasketballPlayer("Karen", 20, 80)
+player_1.display_average()
