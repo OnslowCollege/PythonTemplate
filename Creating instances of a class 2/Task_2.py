ba921c673c1e1c3a9b8ea5abb0a8675ef98b7404
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-02-22 01:43:37 +0000

    Feb 22, 2024, 1:43 AM

--- Creating instances of a class 2/Task_2.py
+++ Creating instances of a class 2/Task_2.py
@@ -5,3 +5,12 @@ Created by: Arkin E
 Date: 20.02.2024
 """
 
+class Movie:
+    def __init__(self, title: str,
+                 director: str, date: int, 
+                 rating: float, genre: str):
+        self.title = title
+        self.director = director
+        self.date = date
+        self.rating = rating
+        self.genre = genre
