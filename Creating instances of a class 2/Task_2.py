73ca2228e32dfadcf87e3aa0cc24566a6c062914
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-02-22 01:48:09 +0000

    Feb 22, 2024, 1:48 AM

--- Creating instances of a class 2/Task_2.py
+++ Creating instances of a class 2/Task_2.py
@@ -15,20 +15,16 @@ class Movie:
         self.box_office_earnings = float(box_office_earnings)
 
     def calculate_net_profit(self):
-        """Calculates and returns the net profit of the movie."""
         self.net_profit = self.box_office_earnings - self.production_cost
         return self.net_profit
 
     def display_data(self):
-        """Displays the movie's details, including calculated net profit."""
         print(f"Title: {self.title}")
         print(f"Release Year: {self.release_year}")
         print(f"Production Cost: ${self.production_cost:.2f}")
         print(f"Box Office Earnings: ${self.box_office_earnings:.2f}")
         print(f"Net Profit: ${self.calculate_net_profit():.2f}")
 
-
-"""Creates and displays Movie objects with calculated net profits."""
 movie1 = Movie("Avatar", 2009, 237000000, 2922917914)
 movie2 = Movie("Avengers: Endgame", 2019, 356000000, 2797501369)
 movie3 = Movie("Titanic", 1997, 200000000, 2229263212)
