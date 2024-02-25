12a4e394ddfd394431b7aa329b2943debe1f3b22
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-02-25 20:19:17 +0000

    Feb 25, 2024, 8:19 PM

--- 04a Functions revision/functions_primm_2.py
+++ 04a Functions revision/functions_primm_2.py
@@ -6,6 +6,13 @@ Date: 26.02.2024
 """
 
 def get_image_bits(width: int, height: int, depth: int):
+    """
+    Calculates the required storage size of an uncompressed image.
+    
+    width - The width of the image in pixels.
+    height - The height of the image in pixels.
+    depth - The bit depth of the image, or the number of bits per pixel.
+    """
     pixels = width * height
     bit_count = pixels * depth
     print(bit_count)
