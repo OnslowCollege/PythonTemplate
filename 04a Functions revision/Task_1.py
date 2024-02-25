d2b251f6deb544e1fce4949dbfc3893654a26347
ArkinEarle <124639509+ArkinEarle@users.noreply.github.com>
2024-02-25 20:15:30 +0000

    Feb 25, 2024, 8:15 PM

---
+++ 04a Functions revision/Task_1.py
@@ -0,0 +1,16 @@
+"""
+Task_1.
+
+Created by: Arkin E
+Date: 26.02.2024
+"""
+
+def get_image_bits(width: int, height: int, depth: int):
+    pixels = width * height
+    bit_count = pixels * depth
+    print(bit_count)
+
+
+
+
+get_image_bits(100, 200, 32)
